5d86fe247e726b02c3f49645e35eafaa11f72d02
AshFri04 <ashleynicole9915@gmail.com>
2018-05-16 03:15:08 +0000

    Hide sign out button -- workin progress.

---
+++ static/glutie.js
@@ -0,0 +1,35 @@
+"use strict"
+
+
+// Excutes js after DOM is loaded
+$(document).ready(function() {
+    console.log('hello ashley');
+
+document.getElementById('login').addEventListener('click', showSignOut());
+
+function showSignOut() {
+    let signout = document.querySelector(".signout");
+    signout.classList.remove("signout-hide");
+    signout.classList.add("signout-show");
+    }
+}) //end of document
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
