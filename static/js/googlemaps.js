c872981a3379a0344f82d690315ccf9dc33a539c
AshFri04 <ashleynicole9915@gmail.com>
2018-05-24 23:25:32 +0000

    Marker Info Windows displays correct information for each restaurant.

--- static/js/googlemaps.js
+++ static/js/googlemaps.js
@@ -1,13 +1,10 @@
 "use strict";  
 
 
-let sanFrancisco = {lat: 37.758129, lng:-122.439012}
+let sanFrancisco = {lat: 37.758129, lng:-122.439012};
 
 let neighborhood_lat = document.querySelector(".hood_lat").value;
 let neighborhood_lng = document.querySelector(".hood_lng").value;
-
-console.log(neighborhood_lat)
-console.log(neighborhood_lng)
 
 let map = new google.maps.Map(document.querySelector('#map'), {
       center: {lat: parseFloat(neighborhood_lat), lng: parseFloat(neighborhood_lng)},
@@ -17,34 +14,53 @@ let map = new google.maps.Map(document.querySelector('#map'), {
 });
 
 let coordinates = document.querySelectorAll(".coordinates");
+let restaurantNames = document.querySelectorAll(".rest_name");
+let restaurantTypes = document.querySelectorAll(".rest_type");
 
 let locationDetails = [];
+let restaurantNameDetails = [];
+let restaurantTypeDetails = [];
 
-for (let coordinate of coordinates){
+for (let coordinate of coordinates) {
     // append is push in js
     // locationDeatail is a list of all coordinates of restaurants in that given neighborhood 
     locationDetails.push(coordinate.value);
 }
+
+for (let restaurantName of restaurantNames) {
+    restaurantNameDetails.push(restaurantName.value);
+
+}
+
+for (let restaurantType of restaurantTypes) {
+    restaurantTypeDetails.push(restaurantType.value);
+
+}
     
 
-for (let i =0; i < (locationDetails.length); i++) {
-    // Change myImageURL to an icon involving eating
+for (let i = 0; i < (locationDetails.length); i++) {
+    // Change myImageURL to an icon involving eating symbol
     let myImageURL = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
     let locationDetail = locationDetails[i].split(' ');
     let latitude = parseFloat(locationDetail[0]);
     let longitude = parseFloat(locationDetail[1]);
+    let restName = restaurantNameDetails[i]
+    let restType = restaurantTypeDetails[i]
+
 
     let restaurant = new google.maps.LatLng(latitude,longitude);
 
     let marker = new google.maps.Marker({
         position: restaurant,
         map: map,
-        title: 'Hover text',
+        title: restName,
         icon: myImageURL
     });
- 
+
+    
     let contentString = '<div id="content">' +
-    '<h1>All my custom content</h1>' +
+    '<h3>'+ restName + '</h3>' +
+    '<h3>'+ restType + '</h3>' +
     '</div>';
 
     let infoWindow = new google.maps.InfoWindow({
