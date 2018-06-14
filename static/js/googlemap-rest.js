917b03f0a82de0b806b61929469770c7831023dc
AshFri04 <ashleynicole9915@gmail.com>
2018-06-14 20:53:04 +0000

    Added styling and google map to search page.

---
+++ static/js/googlemap-rest.js
@@ -0,0 +1,76 @@
+"use strict";  
+
+
+let sanFrancisco = {lat: 37.758129, lng:-122.439012};
+
+
+let map = new google.maps.Map(document.querySelector('#map'), {
+      center: sanFrancisco,
+      zoom:12,
+      mapTypeId: 'roadmap'
+
+});
+
+let coordinates = document.querySelectorAll(".coordinates");
+let restaurantNames = document.querySelectorAll(".rest_name");
+let restaurantTypes = document.querySelectorAll(".rest_type");
+
+let locationDetails = [];
+let restaurantNameDetails = [];
+let restaurantTypeDetails = [];
+
+for (let coordinate of coordinates) {
+    // append is push in js
+    // locationDeatail is a list of all coordinates of restaurants in that given neighborhood 
+    locationDetails.push(coordinate.value);
+}
+
+for (let restaurantName of restaurantNames) {
+    restaurantNameDetails.push(restaurantName.value);
+
+}
+
+for (let restaurantType of restaurantTypes) {
+    restaurantTypeDetails.push(restaurantType.value);
+
+}
+    
+
+for (let i = 0; i < (locationDetails.length); i++) {
+    let locationDetail = locationDetails[i].split(' ');
+    let latitude = parseFloat(locationDetail[0]);
+    let longitude = parseFloat(locationDetail[1]);
+    let restName = restaurantNameDetails[i]
+    let restType = restaurantTypeDetails[i]
+
+
+    let restaurant = new google.maps.LatLng(latitude,longitude);
+
+    let marker = new google.maps.Marker({
+        position: restaurant,
+        map: map,
+        title: restName,
+        icon: '/static/images/map_icon.png'
+    });
+
+    
+    let contentString = '<div id="content">' +
+    '<h3 style="font-family: 'Nanum Myeongjo', serif;">'+ restName + '</h3>' +
+    '<h3>'+ restType + '</h3>' +
+    '</div>';
+
+    let infoWindow = new google.maps.InfoWindow({
+    content: contentString,
+    maxWidth: 200
+    });
+
+    marker.addListener('click', function() {
+    infoWindow.open(map, marker);
+    });
+
+
+
+}
+
+
+
